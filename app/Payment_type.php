e9ebe3e3e3cc5f3ef323854b045c2415db171abb
miglezub <miglezub@gmail.com>
2020-06-03 11:01:16 +0300

    payment type disable timestamps

--- app/Payment_type.php
+++ app/Payment_type.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Payment_type extends Model
 {
     protected $guarded=[];
-
+    public $timestamps = false;
     public function payment()
     {
         return $this->belongsTo(Payment::class);
